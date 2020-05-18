a58ddb1f222c1186d49c034e50f746c6b7664c80
Mark Hunter <m.a.hunter@outlook.com>
2020-05-18 18:59:57 +1000

    Commencing work to utilise a new framework for the UoW/UoWInterchange service.

---
+++ src/main/java/net/fhirbox/pegacorn/ladon/edge/receive/wups/LadonEdgeReceiveEndpointWUP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.fhirbox.pegacorn.ladon.edge.receive.wups;
+
+import net.fhirbox.pegacorn.petasos.wup.processorframework.InterComponentBundleReceiverWUP;
+
+/**
+ *
+ * @author Mark A. Hunter (ACT Health)
+ */
+public class LadonEdgeReceiveEndpointWUP extends InterComponentBundleReceiverWUP
+{
+    
+}
